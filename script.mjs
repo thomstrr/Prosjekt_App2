543e73eb1492d9db1715eaf626a84cc34ecb832d
thomstrr <ths.95@hotmail.com>
2025-01-17 09:00:46 +0100

    Lagt til kode for poem og quote

--- script.mjs
+++ script.mjs
@@ -9,9 +9,33 @@ server.use(express.static('public'));
 
 function getRoot(req, res, next) {
     res.status(HTTP_CODES.SUCCESS.OK).send('Hello World').end();
-}
+};
+
+function getPoem(req, res, next) {
+    const poem = `
+        Roses are red, 
+        Violets are blue, 
+        this is a poem. 
+    `;
+    res.status(HTTP_CODES.SUCCESS.OK).send(`${poem}`).end();
+};
+
+function getQuote(req, res, next) {
+    const quotes = [
+        "You miss 100% of the shots you don’t take. - Wayne Gretzky” - Michael Scott",
+        "You know what they say. Fool me once, strike one, but fool me twice...strike three. - Michael Scott",
+        "When in doubt, look intelligent. - Garrison Keillor",
+        "Hard work never killed anybody, but why take a chance? - Edgar Bergen",
+        "Two things are infinite: the universe and human stupidity; and im not sure about the universe. - Albert Einstein"
+    ];
+
+    const randomQuote = quotes[Math.floor(Math.random() * quotes.length)];
+    res.status(HTTP_CODES.SUCCESS.OK).send(randomQuote).end();
+};
 
 server.get("/", getRoot);
+server.get("/tmp/poem", getPoem);
+server.get("/tmp/quote", getQuote);
 
 server.listen(server.get('port'), function () {
     console.log('server running', server.get('port'));
