15f56cf148b1b578565e5a78fc8b589bc99ba792
thomstrr <ths.95@hotmail.com>
2025-03-07 09:59:38 +0100

    oppdatert kode i sw.js og app.mjs så det blir bedre og enklere å forstå, oppdatert navn på filer

--- public/sw.js
+++ public/sw.js
@@ -1,30 +1,45 @@
-const CACHE_NAME = "pwa-cache-v1";
-const urlsToCache = [
-    "/",
-    "/index.html",
-    "/script.js",
-    "/icon_small.png",
-    "/icon_large.png"
+const cacheID = "workout-tracker-v2";
+const contentToCache = [
+  "/",
+  "/index.html",
+  "/app.mjs",
+  "/icon_small.png",
+  "/icon_large.png",
+  "/css/style.css",
 ];
 
 self.addEventListener("install", (event) => {
-    event.waitUntil(
-        caches.open(CACHE_NAME).then((cache) => cache.addAll(urlsToCache))
-    );
+  console.log("[Service Worker] Install");
+  event.waitUntil(
+    (async () => {
+      const cache = await caches.open(cacheID);
+      console.log("[Service Worker] Caching all: app shell and content");
+      await cache.addAll(contentToCache);
+    })()
+  );
 });
 
 self.addEventListener("fetch", (event) => {
-    event.respondWith(
-        caches.match(event.request).then((response) => response || fetch(event.request))
-    );
+  if (
+    !(
+      event.request.url.startsWith("http:") ||
+      event.request.url.startsWith("https:")
+    )
+  ) { 
+    return;
+}
+
+  event.respondWith(
+    (async () => {
+      const r = await caches.match(event.request);
+      console.log(`[Service Worker] Fetching resource: ${event.request.url}`);
+      if (r) { return r; }
+      const response = await fetch(event.request);
+      const cache = await caches.open(cacheID);
+      console.log(`[Service Worker] Caching new resource: ${event.request.url}`);
+      cache.put(event.request, response.clone());
+      return response;
+    })()
+  );
 });
 
-self.addEventListener("activate", (event) => {
-    event.waitUntil(
-        caches.keys().then((cacheNames) => {
-            return Promise.all(
-                cacheNames.filter((cache) => cache !== CACHE_NAME).map((cache) => caches.delete(cache))
-            );
-        })
-    );
-});
